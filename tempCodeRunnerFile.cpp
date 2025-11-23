da0c6a13e1ee6d18a392d5790996933cfddaecf8
Sie Rormony <niin2015@gmail.com>
2025-11-23 19:42:00 +0700

    .

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "code.hpp"
+
+using namespace std;
+
+int main() {
+    vector<string> teams = {"Team A", "Team B", "Team C", "Team D",
+                            "Team E", "Team F", "Team G", "Team H"};
+
+    BTree tournament;
+    tournament.buildBra(teams);
+
+    // Record results
+    tournament.recRes(1, "Team A");
+    tournament.recRes(2, "Team D");
+    tournament.recRes(3, "Team F");
+    tournament.recRes(4, "Team H");
+    tournament.recRes(5, "Team A");
+    tournament.recRes(6, "Team H");
+    tournament.recRes(7, "Team A"); // Final
+
+      cout << "=== FOOTBALL TOURNAMENT ===\n";
+    tournament.printBracketByRound();
+
+    Node* root = tournament.getRoot();
+    cout << "\n🏆 Overall Winner: " << root->winner << "\n";
+
+    return 0;
+}
